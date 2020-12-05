2bd089d4becfaee48decd29ae9a316f1b6e18c03
Steven Baty <baty_steven@yahoo.com>
2020-12-04 20:34:58 -0700

    UX enchancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -82,15 +82,20 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event, ui) {
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
     console.log(ui);
   },
   deactivate: function(event, ui) {
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
     console.log(ui);
   },
   over: function(event) {
-    console.log(event);
+    $(this).addClass("dropover-active");
   },
   out: function(event) {
+    $(this).removeClass("dropover-active");
     console.log(event);
   },
   update: function() {
@@ -136,9 +141,11 @@ $("#trash").droppable({
     ui.draggable.remove();
   },
   over: function(event, ui) {
+    $(".bottom-trash").addClass("bottom-trash-active");
     console.log(ui);
   },
   out: function(event, ui) {
+    $(".bottom-trash").removeClass("bottom-trash-active");
     console.log(ui);
   }
 });
@@ -162,7 +169,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
